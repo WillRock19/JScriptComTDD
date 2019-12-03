3defefcd6a90e5b9274cb0e257ae7759d974bf86
William Pinto <william.pinto@lambda3.com.br>
2019-12-03 20:50:44 -0300

    Added mocha env configuration to esLint; added reporter to test script in package.json; created html file to make annotations from the Tests topic of the course

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,7 @@ module.exports = {
   },
   rules: {
   },
+  env: {
+    mocha: true
+  }
 };
---
+++ tests/main.spec.js
@@ -0,0 +1,12 @@
+
+describe('Main', () => {
+  context('Caso 1', () => {
+    it('should be vishe', () => { });
+  });
+
+  context('Caso 2', () => {
+    it('should be oloko', () => { });
+
+    it('should be oporra', () => { });
+  });
+});
