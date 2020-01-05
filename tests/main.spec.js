10945b722c9f62a60c9dd6e8fbb5576f33f3fd75
William Pinto <william.pinto@lambda3.com.br>
2020-01-05 11:17:19 -0300

    Criando testes para promise

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -40,12 +40,34 @@ describe('Spotfy Wrapper', () => {
 
   context('generic search', () => {
     const spotFyGetUrl = 'https://api.spotify.com/v1/search';
+    let fetchStub;
+    let promise;
 
-    it('should call the spotfy api', () => {
-      const fetchStub = sinon.stub(global, 'fetch');
+    beforeEach(() => {
+      fetchStub = sinon.stub(global, 'fetch');
+      promise = fetchStub.returnsPromise();
+    });
 
+    afterEach(() => {
+      fetchStub.restore();
+    });
+
+    it('should use fetch to call an endpoint api', () => {
       search();
-      expect(fetchStub).to.been.calledOnceWith(spotFyGetUrl);
+      expect(fetchStub).to.been.calledOnce;
+    });
+
+    it('should call the spotfy api using the parameters in the url', () => {
+      search('Alice Cooper', 'artist');
+      expect(fetchStub).to.been.calledOnceWith(`${spotFyGetUrl}?q=Alice%20Cooper&type=artist`);
+    });
+
+    it('should return an object information fron the endpoint', () => {
+      const expectedResult = { data: 'myData' };
+      promise.resolves(expectedResult);
+
+      const result = search('Alice Cooper', 'artist');
+      expect(result).to.be.eql(expectedResult);
     });
   });
 });
